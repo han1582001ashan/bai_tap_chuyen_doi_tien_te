526cfcfc0dd3fa4a65b873f071319d3ac9f602b8
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-07 18:56:30 +0700

    nop bt

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace USDtoVND{
+    class Program{
+        static void Main(string[] args){
+            float USD; 
+            float VND;
+            const int rate=23000;
+            Console.WriteLine("Type USD: "); 
+            USD = float.Parse(Console.ReadLine());
+            VND = USD * rate;
+            Console.WriteLine("to VND = "+ VND+"đ");
+
+        }
+    }
+}
